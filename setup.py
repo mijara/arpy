be1cc8749e0f9e8b59c3d92a4365c090ab27ebb5
mijara <marjara35@gmail.com>
2017-04-08 21:03:03 -0300

    Rename to EZRPY

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name='arpy',
+setup(name='ezrpy',
       version='0.1.01',
       description='Automatic REST API for Prototypes',
       author='Marcelo Jara',
       author_email='marjara35@gmail.com',
       keywords=['rest', 'prototype', 'api'],
-      url='https://github.com/mijara/arpy',
-      packages=['arpy'],
+      url='https://github.com/mijara/ezrpy',
+      packages=['ezrpy'],
       )
